2a17c8b578cde81b365119b6f1266cf84d96ac18
kmor8 <kamilaoo88@outlook.com>
2021-11-02 12:26:43 -0500

    first commit

---
+++ src/businessLogic/calculadoraPrincipal.java
@@ -0,0 +1,23 @@
+package businessLogic;
+
+public class calculadoraPrincipal {
+	 public static float add(float a, float b)
+	    {
+	        return a+b;
+	    }
+	    
+	    public static float substract(float a, float b)
+	    {
+	        return a-b;
+	    }
+	    
+	    public static float multiply(float a, float b)
+	    {
+	        return a*b;
+	    }
+	    
+	    public static float div(float a, float b)
+	    {
+	        return a/b;
+	    }
+}
