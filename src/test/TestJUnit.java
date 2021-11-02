524a903356ead6eeb93ac36fc3d48b193a2028f9
kmor8 <kamilaoo88@outlook.com>
2021-11-02 13:40:13 -0500

    Se pueden iniciar pruebas

--- src/test/TestJUnit.java
+++ src/test/TestJUnit.java
@@ -1,6 +1,13 @@
 package test;
 
+import businessLogic.calculadoraPrincipal;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import static org.junit.Assert.*;
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 public class TestJUnit {
    @Test
@@ -8,4 +15,78 @@ public class TestJUnit {
       String str= "I am done with Junit setup";
       assertEquals("I am done with Junit setup",str);
    }
+   
+   
+   @BeforeClass
+   public static void setUpClass() {
+   }
+   
+   @AfterClass
+   public static void tearDownClass() {
+   }
+   
+   @Before
+   public void setUp() {
+   }
+   
+   @After
+   public void tearDown() {
+   }
+
+   /**
+    * Test of add method, of class Calculadora.
+    */
+   @Test
+   public void testAdd() {
+       System.out.println("add");
+       float a = 3F;	
+       float b = 5F;
+       float expResult = 8F;
+       float result = calculadoraPrincipal.add(a, b);
+       assertEquals(expResult, result, 0.0);
+   }
+
+   /**
+    * Test of substract method, of class Calculadora.
+    */
+   @Test
+   public void testSubstract() {
+       System.out.println("substract");
+       float a = 10F;
+       float b = 5F;
+       float expResult = 5F;
+       float result = calculadoraPrincipal.substract(a, b);
+       assertEquals(expResult, result, 0.0);
+       
+   }
+
+   /**
+    * Test of multiply method, of class Calculadora.
+    */
+   @Test
+   public void testMultiply() {
+       System.out.println("multiply");
+       float a = 7F;
+       float b = 2F;
+       float expResult = 14F;
+       float result = calculadoraPrincipal.multiply(a, b);
+       assertEquals(expResult, result, 0.0);
+       
+   }
+
+   /**
+    * Test of div method, of class Calculadora.
+    */
+   @Test
+   public void testDiv() {
+       System.out.println("div");
+       float a = 20F;
+       float b = 5F;
+       float expResult = 4F;
+       float result =calculadoraPrincipal.div(a, b);
+       assertEquals(expResult, result, 0.0);
+       
+   }
+   
+
 }
